74720a99999d84ba05723fd1933dfa59dbbf1731
wamunygg <gabriel.wamunyu@passitdown.com>
2020-03-12 16:52:50 -0400

    moved to templates folder and added set command

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,19 @@
 const express = require(`express`);
 const path = require(`path`);
+const hbs = require(`hbs`);
+
 
 const app = express();
 
-
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, `../public`);
+const viewPath = path.join(__dirname, `../templates`);
 
+// Setup handlebars engine and views location
 app.set(`view engine`, `hbs`);
+app.set(`views`, viewPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get(``, (req, res) => { 
