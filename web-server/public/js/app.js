a52db0b4ffc6225698d6047b85772ab9a30e5f19
wamunygg <gabriel.wamunyu@passitdown.com>
2020-03-13 00:37:32 -0400

    submitting user input for search

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,29 @@
-fetch(`http://localhost:3030/weather?address=!`)
-.then(response => {
-    response.json()
-    .then(data => {
-        if(data.error){
-            console.error(`error`, data.error);
-        }else{
-            console.log(`location`, data.location);
-            console.log(`forecast`, data.forecast);
-        }
+const form = document.querySelector(`form`);
+const search = document.querySelector(`input`);
+
+const messageOne = document.querySelector(`#message-1`);
+const messageTwo = document.querySelector(`#message-2`);
+
+
+form.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    
+    const location = search.value;
+
+    messageOne.textContent = `Loading...`;
+    messageTwo.textContent = "";
+
+    fetch(`http://localhost:3030/weather?address=${location}`)
+    .then(response => {
+        response.json()
+        .then(data => {
+            if(data.error){
+                messageOne.textContent = data.error;
+                messageTwo.textContent = "";
+            }else{
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
     })
 })
