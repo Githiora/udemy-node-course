a093b403295656ff93c4d6ca53a3322c73700045
wamunygg <gabriel.wamunyu@passitdown.com>
2020-03-12 23:25:12 -0400

    HTTP requests with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
-console.log(`Client side js file loaded`)
+fetch(`http://localhost:3030/weather?address=!`)
+.then(response => {
+    response.json()
+    .then(data => {
+        if(data.error){
+            console.error(`error`, data.error);
+        }else{
+            console.log(`location`, data.location);
+            console.log(`forecast`, data.forecast);
+        }
+    })
+})
